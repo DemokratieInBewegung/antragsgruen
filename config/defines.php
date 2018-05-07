0b0729fe2fe46a914a8b1510c262ab0905de4014
Tobias Hößl <tobias@hoessl.eu>
2018-05-07 10:52:03 -0500

    Updating

--- config/defines.php
+++ config/defines.php
@@ -6,6 +6,8 @@ use app\models\settings\AntragsgruenApp;
  * @var AntragsgruenApp $params
  */
 
-define('ANTRAGSGRUEN_VERSION', '3.8.2');
+define('ANTRAGSGRUEN_VERSION', '3.8.1');
+#define('ANTRAGSGRUEN_VERSION', '3.8.2');
 define('ANTRAGSGRUEN_HISTORY_URL', 'https://github.com/CatoTH/antragsgruen/blob/v3/History.md');
-define('ANTRAGSGRUEN_UPDATE_BASE', 'https://antragsgruen.de/updates/');
+#define('ANTRAGSGRUEN_UPDATE_BASE', 'https://antragsgruen.de/updates/');
+define('ANTRAGSGRUEN_UPDATE_BASE', 'http://antragsgruen.local/updates/');
